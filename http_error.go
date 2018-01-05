58054da244c2a8333721dfd4afeecaba57fc4efe
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-01-06 00:00:11 +0100

    renamed HttpError -> Error as the prefix is given by the package name

--- http_error.go
+++ http_error.go
@@ -13,22 +13,22 @@ var (
 	NotAuthorized       = NewError(http.StatusUnauthorized)
 )
 
-// HttpError Error containing status code and error
-type HttpError struct {
+// Error Error containing status code and error
+type Error struct {
 	Status int   `json:"status"`
 	Err    error `json:"error"`
 }
 
-// Error Retruns a string representation of an HttpError and
+// Error Retruns a string representation of an Error and
 // makes the type compliant with the go error interface
-func (err HttpError) Error() string {
+func (err Error) Error() string {
 	return fmt.Sprintf("%d - %s", err.Status, err.Err)
 }
 
-// NewError Creates a new HttpError based on a supplied status code
+// NewError Creates a new Error based on a supplied status code
 // attempts to derive the error message
-func NewError(status int) HttpError {
-	return HttpError{
+func NewError(status int) Error {
+	return Error{
 		Status: status,
 		Err:    fmt.Errorf(http.StatusText(status)),
 	}
