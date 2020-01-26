6a67bfa48e624e1f147a6d121e02cb18ca114ccf
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-01-26 15:48:36 +0100

    Added environ and logger packages

---
+++ environ/environ.go
@@ -0,0 +1,30 @@
+package environ
+
+import (
+	"os"
+
+	"github.com/CzarSimon/httputil/logger"
+	"go.uber.org/zap"
+)
+
+var log = logger.MustGetLogger("go24/env", zap.InfoLevel)
+
+// Get gets environment variable with default value.
+func Get(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	return value
+}
+
+// MustGet gets environment variable and panics if not found
+func MustGet(name string) string {
+	value := os.Getenv(name)
+	if value == "" {
+		log.Panic("Failed to get environment variable", zap.String("name", name))
+	}
+
+	return value
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/CzarSimon/httputil
 go 1.13
 
 require (
-	github.com/CzarSimon/util v0.0.0-20171031201240-5db483fbbe25
 	github.com/google/uuid v1.1.1
 	github.com/lib/pq v1.3.0 // indirect
 	github.com/stretchr/testify v1.4.0
+	go.uber.org/zap v1.13.0
 )
